3c1f4dba1b28a6d4d5274bdfc0f3a476902ec544
jonatasjeff <75390366+jonatasjeff@users.noreply.github.com>
2021-03-10 20:23:11 -0300

    Add files via upload

---
+++ adicionais/agendar_so_1.py
@@ -0,0 +1,18 @@
+#RODAR TAREFA
+#pip3 install schedule
+import schedule
+import time
+
+
+def FazertarefaImportante()
+	print('Gerando gra....')
+
+#schedule.cada.tempo.fazer
+schedule.every().monday.do(FazertarefaImportante)
+schedule.every().monday.at("08:00").do(FazertarefaImportante)
+schedule.every().day.at("08:00").do(FazertarefaImportante
+schedule.every(10).seconds.do(FazertarefaImportante)
+
+while 1:
+	schedule.run_pending()
+	time.sleep(1)
